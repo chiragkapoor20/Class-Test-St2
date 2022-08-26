aa6a59f2c78b9894714d6713b6cb333305c162cb
chiragkapoor20 <chirag0175.be20@chitkara.edu.in>
2022-08-26 10:20:23 +0530

    update

---
+++ 1.cpp
@@ -0,0 +1,40 @@
+//write a c++ program to print all the non repeated numbers in an array in minimum time complexity.
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;  //taking number of element in an array
+
+
+    int a[n];     
+
+    for(int i=0;i<n;i++)   // taking input from users
+    {
+        cin>>a[i];
+    }
+
+//Declare the map
+unordered_map<int,int> mp;
+    
+    for(int i=0;i<n;i++){
+        mp[a[i]]++;//for same key increase frequency
+    }
+    
+    //iterator.first == key(element value)
+    //iterator.second == value(frequency)
+cout<<"Repeated number"<<" "<<"Frequency"<<"\n";
+//function to print repeating elements with their frequencies
+    for(auto i:mp)
+    {
+        if(i.second==1)   //frequeny>1 means repeating element
+        {
+        cout<<i.first<<",";    // print repeating elements with their frequencies
+        }
+    }
+    
+
+
+}
---
+++ 2.cpp
@@ -0,0 +1,45 @@
+
+//write a c++ program to print all the repeated no. with frequency in an array in minimum time complexity.
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;   //taking number of element in an array
+
+
+    int a[n];    
+
+    for(int i=0;i<n;i++)   // taking input from users
+    {
+        cin>>a[i];
+    }
+
+//Declare the map
+unordered_map<int,int> mp;
+    
+    for(int i=0;i<n;i++){
+        mp[a[i]]++;//for same key increase frequency
+    }
+    
+    //iterator.first == key(element value)
+    //iterator.second == value(frequency)
+cout<<"Repeated number"<<" "<<"Frequency"<<"\n";
+
+    for(auto i:mp)
+    {
+        if(i.second>1)   //frequeny>1 means repeating element
+        {
+        cout<<i.first<<"                "<<i.second<<"\n"; //print repeating elements with their frequencies
+        }
+    }
+    
+
+
+}
+
+
+
